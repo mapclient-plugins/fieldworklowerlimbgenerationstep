13dda15ad122e12e40fc1253e5ff1950e7cc7e5a
juzhang <jzha263@aucklanduni.ac.nz>
2016-05-18 11:01:19 +1200

    added step location and name to __init__

--- mapclientplugins/fieldworklowerlimbgenerationstep/__init__.py
+++ mapclientplugins/fieldworklowerlimbgenerationstep/__init__.py
@@ -4,6 +4,8 @@ MAP Client Plugin
 '''
 __version__ = '0.1.0'
 __author__ = 'Ju Zhang'
+__stepname__ = 'Fieldwork Lower Limb Generation'
+__location__ = 'https://github.com/mapclient-plugins/fieldworklowerlimbgenerationstep/commits/master.zip'
 
 
 # import class that derives itself from the step mountpoint.
