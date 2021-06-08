8dd8335185d7f9e555b195780bbabba274e11be6
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 13:42:35 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/fieldworklowerlimbgenerationstep/step.py
+++ mapclientplugins/fieldworklowerlimbgenerationstep/step.py
@@ -127,7 +127,7 @@ class FieldworkLowerLimbGenerationStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
